1cafea87af74350d0f752537c3938ad48fe05415
Matt Martin <MattMartin1919@users.noreply.github.com>
2020-06-02 01:05:07 -0400

    adding mocha and chai

---
+++ tests/spec.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const http = require('http');
+const app = require('../app');
+
+const server = http.createServer(app);
+chai.use(chaiHttp);
+chai.should();
+
+
+describe('GET /', () => {
+  it('responds with 200', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
